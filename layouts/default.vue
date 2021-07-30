ceb55e3b7e92553a085e6cefa686362462f70e2f
Oleg Kruchay <okruchay@gmail.com>
2021-07-30 09:28:15 +0300

    feat(layout): adding the default layout.
    v0.1: Basic preparation of the storybook environment.

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <Nuxt />
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Home',
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .container {
+    padding: 0 15px;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
