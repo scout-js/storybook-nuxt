2edcca4c7bf7b4b623bd8a7c2cee9c7e722eee6b
Oleg Kruchay <okruchay@gmail.com>
2021-07-30 09:29:24 +0300

    feat(Sidebar.vue, Table.vue): adding the reusable components.
    v0.1: Basic preparation of the storybook environment.

---
+++ components/Table.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="accordion-table">
+    <Sidebar :sidebar="sidebar" />
+    <ul class="accordion--list content-column">
+      <li v-for="(item, key) in items" :key="key" class="accordion--item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Table',
+    props: {
+      sidebar: {
+        type: Array,
+        required: true,
+      },
+      items: {
+        type: Array,
+        required: true,
+      },
+    },
+  }
+</script>
