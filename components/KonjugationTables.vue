92d06c250041935a4dafc6b19fce3d3c01c12179
Oleg Kruchay <okruchay@gmail.com>
2021-07-30 10:12:21 +0300

    feat(DeklinationTables.vue, ConjugationTables.vue): adding the components with combined sidebars and tables.
    v0.1: Basic preparation of the storybook environment.

---
+++ components/KonjugationTables.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="accordion-wrapper conjugation">
+    <h3 class="accordion-tables_header">{{ 'Konjugation ///// Gemischte Beugung (mit ein, kein, Possessiv-pronomen u.a.)' }}</h3>
+    <div class="accordion-tables">
+      <div class="accordion-tables_group">
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.prasens" />
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.prateritum" />
+      </div>
+      <div class="accordion-tables_group">
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.perfekt" />
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.plusquamperfekt" />
+      </div>
+      <div class="accordion-tables_group">
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.futurI" />
+        <Table :sidebar="konjugation.sidebar" :items="konjugation.futurIi" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {konjugation} from "~/static/data.json"
+  export default {
+    name: 'KonjugationTables',
+    data() {
+      return {
+        konjugation,
+      }
+    },
+  }
+</script>
