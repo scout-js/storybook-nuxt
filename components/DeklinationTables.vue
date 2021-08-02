574518f6742f8e49cf54b2866e245dcef73e0d05
Oleg Kruchay <okruchay@gmail.com>
2021-08-02 18:17:05 +0300

    feat(DeklinationTables.vue, KonjugationTables.vue): updating the markup for hints and groups.
    v0.2.1: Updating the styles and markup for Tables.

--- components/DeklinationTables.vue
+++ components/DeklinationTables.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="accordion-wrapper deklination">
     <h3 class="accordion-tables_header">{{ 'Deklination // Gemischte Beugung (mit ein, kein, Possessiv-pronomen u.a.)' }}</h3>
-    <div class="accordion-tables">
+    <div class="accordion-groups">
       <div class="accordion-tables_group">
         <Table class="table--maskulin" :sidebar="deklination.sidebar" :items="deklination.maskulin" />
         <Table class="table--femininum" :sidebar="deklination.sidebar" :items="deklination.femininum" />
       </div>
-      <li class="accordion--item_hint">{{ deklination.hint }}</li>
       <div class="accordion-tables_group">
         <Table class="table--neutrum" :sidebar="deklination.sidebar" :items="deklination.neutrum" />
         <Table class="table--plural" :sidebar="deklination.sidebar" :items="deklination.plural" />
       </div>
-      <li class="accordion--item_hint"></li>
     </div>
+    <div class="accordion--item_hint">{{ deklination.hint }}</div>
   </div>
 </template>
 
--- components/KonjugationTables.vue
+++ components/KonjugationTables.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="accordion-wrapper conjugation">
     <h3 class="accordion-tables_header">{{ 'Konjugation ///// Gemischte Beugung (mit ein, kein, Possessiv-pronomen u.a.)' }}</h3>
-    <div class="accordion-tables">
-      <div class="accordion-tables_group">
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.prasens" />
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.prateritum" />
+    <div class="accordion-groups">
+      <div class="accordion-group">
+        <div class="accordion-tables_group">
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.prasens" />
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.prateritum" />
+        </div>
+        <div class="accordion--item_hint">{{ konjugation.hint }}</div>
       </div>
-      <li class="accordion--item_hint"></li>
-      <div class="accordion-tables_group">
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.perfekt" />
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.plusquamperfekt" />
+      <div class="accordion-group">
+        <div class="accordion-tables_group">
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.perfekt" />
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.plusquamperfekt" />
+        </div>
+        <div class="accordion--item_hint">{{ konjugation.hint }}</div>
       </div>
-      <li class="accordion--item_hint">{{ konjugation.hint }}</li>
-      <div class="accordion-tables_group">
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.futurI" />
-        <Table :sidebar="konjugation.sidebar" :items="konjugation.futurIi" />
+      <div class="accordion-group">
+        <div class="accordion-tables_group">
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.futurI" />
+          <Table :sidebar="konjugation.sidebar" :items="konjugation.futurIi" />
+        </div>
+        <div class="accordion--item_hint">{{ konjugation.hint }}</div>
       </div>
-      <li class="accordion--item_hint">{{ konjugation.hint }}</li>
     </div>
   </div>
 </template>
