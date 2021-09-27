90752ecd27831db344d016a1ead3bf6495a2a08b
Oleg Kruchay <okruchay@gmail.com>
2021-09-27 15:28:17 +0300

    feat(DeklinationNounSingle{.vue, -Tables.vue}): adding the component DeklinationNounSingle.
    v0.2.8.1: Creates the new component DeklinationNounSingle and derivatives updates.

---
+++ components/DeclinationNounSingleTables.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="con-dec__wrapper declination single-group partly">
+    <h3 v-if="id && title" :id="id" class="accordion-tables__header">{{ title }}</h3>
+    <div class="accordion-content">
+      <div class="accordion-groups">
+        <div class="accordion-tables__group">
+          <Table :sidebar="declination.sidebar" :items="declination.singular" />
+        </div>
+      </div>
+      <div class="accordion-hints__group">
+        <div class="accordion__item__hint"><sup>(1)</sup> <span>{{ declination.hint }}</span></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { declination } from '~/static/data.json'
+  import Table from '~/components/Table'
+
+  export default {
+    name: 'DeclinationNounSingleTables',
+    components: { Table },
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        declination,
+      }
+    },
+  }
+</script>
---
+++ components/DeclinationNounsSingle.vue
@@ -0,0 +1,23 @@
+<template>
+  <section data-block-type="grammar">
+    <h2 id="grammatik">{{ 'Grammatik' }}</h2>
+    <div id="accordion">
+      <DeclinationNounSingleTables id="deklination" title="Deklination" />
+    </div>
+  </section>
+</template>
+
+<script>
+import { declination } from '~/static/data.json'
+import DeclinationNounSingleTables from '~/components/DeclinationNounSingleTables'
+
+export default {
+  name: 'DeclinationSingleNouns',
+  components: { DeclinationNounSingleTables },
+  data() {
+    return {
+      declination,
+    }
+  },
+}
+</script>
